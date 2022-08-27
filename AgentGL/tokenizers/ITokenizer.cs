a5964033a67240f47634396d48db07a1610e65a3
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-26 22:57:23 -0400

    added tokenizer with custom interface

---
+++ AgentGL/tokenizers/ITokenizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentGL.Parsing.Tokenizers
+{
+    public interface ITokenizer
+    {
+        IEnumerable<Parsing.Tokens.AgentGLToken> Tokenize(string queryDsl);
+    }
+}
---
+++ AgentGL/tokenizers/RegexTokenizer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using AgentGL.Parsing.Tokens;
+
+namespace AgentGL.Parsing.Tokenizers
+{
+    public class RegexTokenizer : ITokenizer
+    {
+        private List<TokenDefinition> _tokenDefinitions;
+
+        public RegexTokenizer()
+        {
+            _tokenDefinitions = new List<TokenDefinition>();
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.And, "and", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Application, "app|application", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Between, "between", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.CloseParenthesis, "\\)", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Comma, ",", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Equals, "=", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.ExceptionType, "ex|exception", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Fingerprint, "fingerprint", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.NotIn, "not in", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.In, "in", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Like, "like", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Limit, "limit", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Match, "match", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Message, "msg|message", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.NotEquals, "!=", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.NotLike, "not like", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.OpenParenthesis, "\\(", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Or, "or", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.StackFrame, "sf|stackframe", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.DateTimeValue, "\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d", 2));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.StringValue, "'([^']*)'", 1));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Number, "\\d+", 2));
+        }
+
+
+    }
+}
