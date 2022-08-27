739bc7c81e802c950698f9f12afedb6657d9a32b
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-27 10:52:15 -0400

    added Commons library

---
+++ AgentGL/AgentGL.Common/Datatype.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentGL.Common
+{
+    public enum Datatype
+    {
+        NONE = 0,
+        BOOL,
+        STRING,
+        INT,
+        DEC,
+        OBJECT,
+        PROTO,
+        STRUCT,
+        LAMBDA
+    }
+}
---
+++ AgentGL/AgentGL.Common/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace AgentGL.Common.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<K> Map<T, K>(this IEnumerable<T> source, Func<T, K> action)
+        {
+            foreach (var x in source)
+            {
+                yield return action(x);
+            }
+        }
+
+        public static S Filter<T, S>(this IEnumerable<T> source, Func<S, T, S> accumulator, S initialAccumulator)
+        {
+            var init = initialAccumulator;
+            foreach (var x in source)
+            {
+                init = accumulator(init, x);
+            }
+
+            return init;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var x in source)
+            {
+                action(x);
+            }
+        }
+    }
+}
