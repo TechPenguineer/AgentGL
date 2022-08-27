ba108c4913408a3f8d3c03071fa71d895d594d78
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-27 10:41:49 -0400

    Project reset

--- AgentGL/runtime.cs
+++ AgentGL/runtime.cs
@@ -5,11 +5,7 @@ namespace AgentGL.runtime
     {
         public static void Main(string[] args)
         {
-            dynamic data = Parser.parser.Parse(args[0]);
-            foreach(var x in data)
-            {
-                Console.WriteLine(x);
-            }
+
         }
     }
 }
