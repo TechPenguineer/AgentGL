fdc4916d7fd6a0a5422c54e26b8a3ae3ec30e38f
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-08-26 22:44:32 -0400

    added base token class

---
+++ AgentGL/parser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentGL.Parser
+{
+    public class parser
+    {
+        public static dynamic Parse(string file)
+        {
+            string text = System.IO.File.ReadAllText(file);
+            return text.Split();
+        }
+
+    }
+}
--- AgentGL/runtime.cs
+++ AgentGL/runtime.cs
@@ -1,10 +1,15 @@
-﻿namespace AgentGL.runtime
+﻿using Figgle;
+namespace AgentGL.runtime
 {
     public class AgentGL
     {
         public static void Main(string[] args)
         {
-
+            dynamic data = Parser.parser.Parse(args[0]);
+            foreach(var x in data)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
